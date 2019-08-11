87c9ab889d10fa26fb008727b0dd724b1e7dfe5b
Igor Muchychka <mucsi_96@yahoo.com>
2019-08-11 02:04:02 +0200

    Add test for Beer component

--- src/core/Beer.test.tsx
+++ src/core/Beer.test.tsx
@@ -1,30 +1,50 @@
-import React, { DOMElement } from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import React from 'react';
+import { render } from '@testing-library/react';
 import { Beer } from './Beer';
 
-let container: HTMLDivElement | null = null;
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  if (container) {
-    unmountComponentAtNode(container);
-    container.remove();
-  }
-  container = null;
-});
+const renderWithProps = (props: Partial<React.ComponentProps<typeof Beer>> = {}) =>
+  render(
+    <Beer id={1} name="test beer" imageUrl="test/image/url.png" tagline="test tagline" {...props} />
+  );
 
 describe('Beer', () => {
   it('renders', () => {
-    act(() => {
-      render(
-        <Beer id={1} name="test beer" imageUrl="test/image/url.png" tagline="test tagline" />,
-        container
-      );
-    });
-    expect(container && container.innerHTML).toBe('Hey, stranger');
+    const { container } = renderWithProps();
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <a
+          class="container container"
+          href="/beer/1"
+        >
+          <img
+            alt="test beer"
+            class="image"
+            src="test/image/url.png"
+          />
+          <div>
+            <h2
+              class="title"
+            >
+              test beer
+            </h2>
+            <p>
+              test tagline
+            </p>
+          </div>
+        </a>
+      </div>
+    `);
+  });
+
+  it('renders star icon if isFavorite', () => {
+    const { getByText } = renderWithProps({ isFavorite: true });
+    expect(getByText('star.svg')).toMatchInlineSnapshot(`
+      <svg
+        aria-labelledby="favorite"
+        class="favoriteMark"
+      >
+        star.svg
+      </svg>
+    `);
   });
 });
--- src/core/Beer.tsx
+++ src/core/Beer.tsx
@@ -17,7 +17,7 @@ export const Beer: React.FC<TBeerProps> = ({ id, name, imageUrl, tagline, isFavo
     <div>
       <h2 className={style.title}>
         {name}
-        {isFavorite && <StarIcon className={style.favoriteMark} />}
+        {isFavorite && <StarIcon className={style.favoriteMark} aria-labelledby="favorite" />}
       </h2>
       <p>{tagline}</p>
     </div>
